7838d74d75203741d095a7ec1f799d20ef2d5da4
Kyle Zheng <kylezheng73@gmail.com>
2024-04-04 23:37:04 -0400

    encode_alphanumeric

--- src/qr.rs
+++ src/qr.rs
@@ -93,6 +93,43 @@ fn encode_numeric(qrcode: &mut QRCode, input: &str) {
     );
 }
 
+fn ascii_to_b45(c: u8) -> u8 {
+    match c {
+        x if x >= b'A' => x - b'A' + 10,
+        b':' => 44,
+        x if x >= b'0' => x - b'0',
+        b' ' => 36,
+        b'$' => 37,
+        b'%' => 38,
+        b'*' => 39,
+        b'+' => 40,
+        b'-' => 41,
+        b'.' => 42,
+        b'/' => 43,
+        _ => unreachable!("Not b45 encodable"),
+    }
+}
+fn encode_alphanumeric(qrcode: &mut QRCode, input: &str) {
+    qrcode.push_bits(0b0010, 4);
+
+    let input = input.as_bytes();
+
+    for i in 0..(input.len() / 2) {
+        let group =
+            ascii_to_b45(input[i * 2]) as usize * 45 + ascii_to_b45(input[i * 2 + 1]) as usize;
+        qrcode.push_bits(group, 11);
+    }
+
+    if (input.len() & 1) == 1 {
+        qrcode.push_bits(ascii_to_b45(input[input.len() - 1]).into(), 6);
+    }
+
+    qrcode.push_bits(
+        input.len(),
+        bits_char_count_indicator(qrcode.version, Mode::Alphanumeric),
+    );
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -129,6 +166,30 @@ mod tests {
         assert_eq!(
             qrcode.data,
             get_data_vec("0001 0001111011 0111001000 0000000110")
+        );
+    }
+
+    #[test]
+    fn encode_alphanumeric_works() {
+        let mut qrcode = QRCode {
+            data: Vec::new(),
+            mask: 0,
+            version: 1,
+            ecc: ECL::Low,
+        };
+
+        encode_alphanumeric(&mut qrcode, "1");
+        assert_eq!(qrcode.data, get_data_vec("0010 000001 000000001"));
+
+        qrcode.data = Vec::new();
+        encode_alphanumeric(&mut qrcode, "99");
+        assert_eq!(qrcode.data, get_data_vec("0010 00110011110 000000010"));
+
+        qrcode.data = Vec::new();
+        encode_alphanumeric(&mut qrcode, "ABC1::4");
+        assert_eq!(
+            qrcode.data,
+            get_data_vec("0010 00111001101 01000011101 11111101000 000100 000000111")
         );
     }
 }
